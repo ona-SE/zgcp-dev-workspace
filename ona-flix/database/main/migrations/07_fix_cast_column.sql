95143838f3be440184e0781281691115173037db
jrcoak <josephrcoakley@gmail.com>
2025-12-12 19:10:59 +0000

    Revert "Integrate OnaFlix application into workspace"
    This reverts commit 4aeb94ebce7cc48aafee6bd76d3f222c65388e64.
    
    Co-authored-by: Ona <no-reply@ona.com>

--- ona-flix/database/main/migrations/07_fix_cast_column.sql
+++
@@ -1,6 +0,0 @@
--- Fix the cast column name issue (cast is a reserved word in PostgreSQL)
-ALTER TABLE movies RENAME COLUMN cast TO movie_cast;
-
--- Update the index
-DROP INDEX IF EXISTS idx_movies_cast;
-CREATE INDEX IF NOT EXISTS idx_movies_movie_cast ON movies USING gin(movie_cast);
